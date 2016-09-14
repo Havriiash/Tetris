b0c3b4798b7f2d1a7adfd6b9e9bf74fd82d04439
unknown <write.dvg@gmail.com>
2016-09-14 21:43:39 +0300

    added basic files and activities for application

---
+++ app/src/main/java/com/tetris/dmitriy/tetris/MainActivity.java
@@ -0,0 +1,53 @@
+package com.tetris.dmitriy.tetris;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.ActivityMain_NewGameBtn).setOnClickListener(onNewGameBtnClick);
+        findViewById(R.id.ActivityMain_SettingsBtn).setOnClickListener(onSettingsBtnClick);
+        findViewById(R.id.ActivityMain_RecordsBtn).setOnClickListener(onRecordsBtnClick);
+        findViewById(R.id.ActivityMain_ExitBtn).setOnClickListener(onExitBtnClick);
+    }
+
+    private View.OnClickListener onNewGameBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
+
+    private View.OnClickListener onSettingsBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
+
+    private View.OnClickListener onRecordsBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
+
+    private View.OnClickListener onExitBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        android.os.Process.killProcess(android.os.Process.myPid());
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/tetris/dmitriy/tetris/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.tetris.dmitriy.tetris;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    private static final int DELAY = 5 * 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent startMainActivityIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(startMainActivityIntent);
+            }
+        }, DELAY);
+    }
+}
