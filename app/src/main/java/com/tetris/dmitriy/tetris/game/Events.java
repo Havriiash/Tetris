af38d994f9a6247684fac623a88e1067445d89c1
unknown <write.dvg@gmail.com>
2016-09-20 02:11:32 +0300

    events that can occur with figures

---
+++ app/src/main/java/com/tetris/dmitriy/tetris/game/Events.java
@@ -0,0 +1,12 @@
+package com.tetris.dmitriy.tetris.game;
+
+/**
+ * Created by Dmitriy on 18.09.2016.
+ */
+public enum Events {
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    MOVE_DOWN,
+    FALL_DOWN,
+    ROTATE
+}
