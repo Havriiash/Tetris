ad30d11d05939be1895d73113d09bbe7b3212ea9
unknown <write.dvg@gmail.com>
2016-09-20 23:46:47 +0300

    class for store message types for GlassController

---
+++ app/src/main/java/com/tetris/dmitriy/tetris/game/MessageTypes.java
@@ -0,0 +1,13 @@
+package com.tetris.dmitriy.tetris.game;
+
+/**
+ * Created by Dmitriy on 20.09.2016.
+ */
+
+public class MessageTypes {
+    public static final int SCORES = 0;
+    public static final int LEVEL = 1;
+    public static final int NEXT_FIGURE = 2;
+    public static final int REFRESH = 3;
+    public static final int CLEAR_LINE = 4;
+}
