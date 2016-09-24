8e7bca13c03ba761bd08244c5b1f1cfba9027041
unknown <write.dvg@gmail.com>
2016-09-24 20:49:49 +0300

    added game over message

--- app/src/main/java/com/tetris/dmitriy/tetris/game/MessageTypes.java
+++ app/src/main/java/com/tetris/dmitriy/tetris/game/MessageTypes.java
@@ -10,4 +10,5 @@ public class MessageTypes {
     public static final int NEXT_FIGURE = 2;
     public static final int REFRESH = 3;
     public static final int CLEAR_LINE = 4;
+    public static final int GAME_OVER = 5;
 }
