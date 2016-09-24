e83aabf32bfb5aa57f3232e8cc57d87cee6ccb7d
unknown <write.dvg@gmail.com>
2016-09-24 20:48:46 +0300

    added crashlytics

--- app/src/main/java/com/tetris/dmitriy/tetris/MainActivity.java
+++ app/src/main/java/com/tetris/dmitriy/tetris/MainActivity.java
@@ -1,19 +1,21 @@
 package com.tetris.dmitriy.tetris;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.ActivityMain_NewGameBtn).setOnClickListener(onNewGameBtnClick);
-        findViewById(R.id.ActivityMain_SettingsBtn).setOnClickListener(onSettingsBtnClick);
         findViewById(R.id.ActivityMain_RecordsBtn).setOnClickListener(onRecordsBtnClick);
         findViewById(R.id.ActivityMain_ExitBtn).setOnClickListener(onExitBtnClick);
     }
@@ -23,13 +25,6 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             Intent startGameIntent = new Intent(MainActivity.this, GameActivity.class);
             startActivity(startGameIntent);
-        }
-    };
-
-    private View.OnClickListener onSettingsBtnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-
         }
     };
 
@@ -52,4 +47,5 @@ public class MainActivity extends AppCompatActivity {
         android.os.Process.killProcess(android.os.Process.myPid());
         super.onDestroy();
     }
+
 }
