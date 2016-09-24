bba0aaa9fd3e2e5565da43cdd9a7e8bd9700017f
unknown <write.dvg@gmail.com>
2016-09-24 20:48:59 +0300

    added GameOver method

--- app/src/main/java/com/tetris/dmitriy/tetris/game/GlassController.java
+++ app/src/main/java/com/tetris/dmitriy/tetris/game/GlassController.java
@@ -8,11 +8,11 @@ import com.tetris.dmitriy.tetris.game.figures.Figure;
  */
 
 public interface GlassController {
-
     void onChangeScores(int scores);
     void onChangeLevel(int level);
     void onChangeNextFigure(Figure figure);
     void onClearLines(int yLine, int clearLinesCount);
 
     void onRefresh();
+    void onGameOver();
 }
